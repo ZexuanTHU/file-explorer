fe944a0100b4ba2821b6dfd1da5974aee7dbdf8e
Shian <shianyip@gmail.com>
2018-02-03 03:46:30 +0800

    style: auto clean code
    jslint standard style

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 var fs = require('fs'),
     stdin = process.stdin,
-    stdout = process.stdout
+    stdout = process.stdout;
 
 /**
  * main function
@@ -12,22 +12,22 @@ var fs = require('fs'),
 
 
 fs.readdir(process.cwd(), function (err, files) {
-    console.log('')
+    console.log('');
 
     if (!files.length) {
         return console.log('    \033[31m No files to show!033[39m\n')
     }
 
-    console.log('   Select which file or directory you want to see\n')
+    console.log('   Select which file or directory you want to see\n');
 
-    var stats = []
+    var stats = [];
 
     // called for each file walked in the directory
     function file(i) {
-        var filename = files[i]
+        var filename = files[i];
 
         fs.stat(process.cwd() + '/' + filename, function (err, stat) {
-            stats[i] = stat
+            stats[i] = stat;
 
             if (stat.isDirectory()) {
                 console.log('       ' + i + '   \033[36m' + filename + '\033[39m')
@@ -35,8 +35,8 @@ fs.readdir(process.cwd(), function (err, files) {
                 console.log('       ' + i + '   \033[90m' + filename + '\033[39m')
             }
 
-            if (++i == files.length) {
-                console.log('')
+            if (++i === files.length) {
+                console.log('');
                 read()
             } else {
                 file(i)
@@ -46,37 +46,37 @@ fs.readdir(process.cwd(), function (err, files) {
 
     // read user input when files are shown
     function read() {
-        console.log('')
-        stdout.write('  \033[33mEnter your choice: \033[39m')
-        stdin.resume()
-        stdin.setEncoding('utf8')
+        console.log('');
+        stdout.write('  \033[33mEnter your choice: \033[39m');
+        stdin.resume();
+        stdin.setEncoding('utf8');
         stdin.on('data', option)
     }
 
     // called with the option supplied by the user
     function option(data) {
-        var filename = files[Number(data)]
+        var filename = files[Number(data)];
 
         if (!filename) {
             stdout.write('  \033[31mEnter your choice:  \033[39m')
         } else {
-            stdin.pause()
+            stdin.pause();
 
             if (stats[Number(data)].isDirectory()) {
                 fs.readdir(process.cwd() + '/' + filename, function (err, files) {
-                    console.log('')
-                    console.log('   (' + files.length + ' files)')
+                    console.log('');
+                    console.log('   (' + files.length + ' files)');
                     files.forEach(function (file) {
                         console.log('   -  ' + file)
-                    })
-                    console.log('')
+                    });
+                    console.log('');
 
                     exit()
                 })
             } else {
                 fs.readFile(process.cwd() + '/' + filename, 'utf8', function (err, data) {
-                    console.log('')
-                    console.log('\033[90m' + data.replace(/(.*)/g, '    $1') + '\033[39m')
+                    console.log('');
+                    console.log('\033[90m' + data.replace(/(.*)/g, '    $1') + '\033[39m');
 
                     exit()
                 })
@@ -85,12 +85,12 @@ fs.readdir(process.cwd(), function (err, files) {
     }
 
     function exit() {
-        console.log('Exit...')
+        console.log('Exit...');
         process.exit(0)
     }
 
     file(0)
-})
+});
 
 
 
